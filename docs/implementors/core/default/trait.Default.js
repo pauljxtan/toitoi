3eddcbbbff9263c3f266852489f4c1e97afc1f04
Paul Tan <pjxtan@gmail.com>
2020-09-27 12:29:49 -0400

    Nevermind

--- docs/implementors/core/default/trait.Default.js
+++ docs/implementors/core/default/trait.Default.js
@@ -1,3 +1,10 @@
 (function() {var implementors = {};
-implementors["toitoi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/player/struct.Player.html\" title=\"struct toitoi::player::Player\">Player</a>","synthetic":false,"types":["toitoi::player::Player"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/tile/struct.Tile.html\" title=\"struct toitoi::tile::Tile\">Tile</a>","synthetic":false,"types":["toitoi::tile::Tile"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/types/struct.HandContext.html\" title=\"struct toitoi::types::HandContext\">HandContext</a>","synthetic":false,"types":["toitoi::types::HandContext"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+implementors["toitoi"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/player/struct.Player.html\" title=\"struct toitoi::player::Player\">Player</a>",synthetic:false,types:["toitoi::player::Player"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/tile/struct.Tile.html\" title=\"struct toitoi::tile::Tile\">Tile</a>",synthetic:false,types:["toitoi::tile::Tile"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"toitoi/types/struct.HandContext.html\" title=\"struct toitoi::types::HandContext\">HandContext</a>",synthetic:false,types:["toitoi::types::HandContext"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
