2ad49e850b3d7283f55752677e64ea5a79dc2053
Paul Tan <pjxtan@gmail.com>
2020-09-27 12:28:02 -0400

    Update docs

--- docs/aliases.js
+++
@@ -1,4 +0,0 @@
-var ALIASES = {};
-ALIASES["either"] = {};
-ALIASES["itertools"] = {};
-ALIASES["toitoi"] = {};
--- docs/implementors/core/hash/trait.Hash.js
+++ docs/implementors/core/hash/trait.Hash.js
@@ -1,11 +1,4 @@
 (function() {var implementors = {};
-implementors["either"] = [{text:"impl&lt;L:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>, R:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt;",synthetic:false,types:["either::Either"]},];
-implementors["toitoi"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"toitoi/tile/struct.Tile.html\" title=\"struct toitoi::tile::Tile\">Tile</a>",synthetic:false,types:["toitoi::tile::Tile"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"toitoi/types/enum.FuReason.html\" title=\"enum toitoi::types::FuReason\">FuReason</a>",synthetic:false,types:["toitoi::types::FuReason"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["either"] = [{"text":"impl&lt;L:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>, R:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt;","synthetic":false,"types":["either::Either"]}];
+implementors["toitoi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"toitoi/tile/struct.Tile.html\" title=\"struct toitoi::tile::Tile\">Tile</a>","synthetic":false,"types":["toitoi::tile::Tile"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"toitoi/types/enum.FuReason.html\" title=\"enum toitoi::types::FuReason\">FuReason</a>","synthetic":false,"types":["toitoi::types::FuReason"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
